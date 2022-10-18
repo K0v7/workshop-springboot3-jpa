d50c8c1a3825472d1f2c087ef242ad51bfe3865e
Felipe Silva <contato.felipesilva2020@Hotmail.com>
2022-10-18 17:57:33 -0300

    Adding comments on code for each functionality on code - PT 1

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -43,9 +43,13 @@ public class TestConfig implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		// Inserindo Categorias No nosso banco de dados
+
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
+
+		// inserindo Produtos no nosso banco de dados
 
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
@@ -53,8 +57,12 @@ public class TestConfig implements CommandLineRunner {
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
+		// salvando os produtos e as categorias inseridas no banco
+
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		// determinando qual categoria é de cada produro
 
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
@@ -63,28 +71,45 @@ public class TestConfig implements CommandLineRunner {
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
 
+		// salvando as definições de categoria no banco
+
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		// inserindo usuários no Banco
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+		// inserindo pedidos feito por usuários no banco
 
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
+		// salvando os pedidos feitos pelos usuários
+
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+		// pegando os preõs pagos or cada pedido feito pelos usuários
 
 		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
+		// salva os pedidos feitos pelo usuários
+
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-		
-		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+
+		// registro de pagamento feito por um usuário, que estava como "Aguardando
+		// pagamento"
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
 		o1.setPayment(pay1);
-		
+
+		// Registrando o pagamento
+
 		orderRepository.save(o1);
 	}
 
